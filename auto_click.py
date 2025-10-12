49bf361544306271c21292eb374a1184d883bb22
zkaits <zkaits@163.com>
2025-10-12 21:54:01 +0800

    Update auto_click.py

--- auto_click.py
+++ auto_click.py
@@ -56,7 +56,7 @@ clean_old_logs()
 
 # 主逻辑开始
 try:
-    driver.get("https://onlyno999.streamlit.app/")
+    driver.get("https://o2skygg.streamlit.app/")
     print("已打开网页，等待页面加载 30 秒...")
     time.sleep(30)  # 初次加载等待
 
